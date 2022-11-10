060781776a7332c2f28858776819cd5110476d4f
Helena Chueva <nefir_7_62@mail.ru>
2022-11-10 21:55:02 +0100

    making thread server

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -4,8 +4,15 @@
 
 import http.server
 import os
+import threading
+from socketserver import ThreadingMixIn
+from urllib.parse import parse_qs, unquote
+
 import requests
-from urllib.parse import unquote, parse_qs
+
+
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
 
 memory = {}
 
@@ -109,5 +116,5 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
